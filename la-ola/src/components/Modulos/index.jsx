a2cb6a3a004c6c90a9cdb86a7f6e7b23be6519f4
Enrique <quique.green92@gmail.com>
2022-12-12 18:10:45 -0300

    Update index.jsx

--- la-ola/src/components/Modulos/index.jsx
+++ la-ola/src/components/Modulos/index.jsx
@@ -4,7 +4,7 @@ import Slider from '../Slider/index';
 import {SliderData} from '../Slider/SliderData';
 import Dibujo from '../../assets/doodle.svg'
 
-import ModulosSvg from '../../assets/modulos/modulos.svg';
+
 
 import {Fade} from 'react-awesome-reveal';
 
@@ -15,54 +15,41 @@ const Modulos = () => {
   return (
     <Fade triggerOnce>
     <div className="bg-[url('/src/assets/backgroundModulos.png')] bg-cover w-screen h-auto object-fill text-white">
-      <div className='hidden md:inline-grid w-screen h-screen'>
-        <div className='p-4 grid grid-cols-2 w-screen items-center '>
-          <div className='mx-auto'>
-            <Fade triggerOnce cascade direction='left'>
-            <div className='p-4 md:max-w-md'>  
-                    <span className='font-BoldItalic text-base lg:text-xl '>{t("modulos.title1")}</span>
-                    <p className='font-TTNormsLight text-sm lg:text-base'>{t("modulos.sen1")}</p>
-                  </div>
-                  <div className='p-4 md:max-w-md '>  
-                    <span className='font-BoldItalic text-base lg:text-xl '>{t("modulos.title2")}</span>
-                    <p className='font-TTNormsLight text-sm lg:text-base'>{t("modulos.sen2")}</p>
-                  </div>
-                 
-                  </Fade>
-            </div>
-            <Fade triggerOnce>
-            <div className='mx-auto'>
-          
-            
-              <img src={Dibujo} alt='Dibujo' className=' mx-auto'/>  
-              
-            </div>
-          </Fade>
-           
-          </div>
-         
+   
+      <div className='hidden md:inline-grid w-screen h-screen place-items-center'>
+        <div className='container pb-2 flex flex-col-reverse items-center px-6-mx-auto space-y-0 md:space-y-0 px-6 mx-auto s'>
+                  <div className='flex flex-col  space-y-8 text-white font-TTNormsReg'>
+                    <Fade direction='left' delay={2} triggerOnce cascade>
+                      <h1 className='max-w-lg  text-center font-BoldItalic text-base lg:text-xl'>{t("modulos.title1")}</h1>
+                      <p className='max-w-md text-center text-sm lg:text-base font-TTNormsLight'>{t("modulos.sen1")} </p>
+                      <h1 className='max-w-lg  text-center font-BoldItalic text-base lg:text-xl    '>{t("modulos.title2")}</h1>
+                      <p className='max-w-md text-sm text-center lg:text-base font-TTNormsLight'>{t("modulos.sen2")} </p> 
+                    </Fade> 
+                  </div> 
+                
+        </div>
+       
+      
       
       </div>
       <Fade triggerOnce>
-      <div className='min-w-full gap-1 grid items p-2 grid-rows-3 md:hidden' >
+      <div className='min-w-full gap-1 grid items p-2 grid-rows-2 md:hidden place-content-center place-items-center text-center'  >
         <Fade cascade triggerOnce direction='left'>
-          <div className='p-3 md:max-w-md '>  
+          <div className='p-3 md:max-w-md space-y-8 '>  
             <span className='font-BoldItalic text-base lg:text-xl'>{t("modulos.title1")}</span>
             <p className='font-TTNormsLight text-sm lg:text-base'>{t("modulos.sen1")}</p>
           </div>
-          <div className=' p-3 md:max-w-md'>  
+          <div className=' p-3 md:max-w-md space-y-8'>  
             <span className='font-BoldItalic text-base lg:text-xl'>{t("modulos.title2")}</span>
             <p className='font-TTNormsLight text-sm lg:text-base '>{t("modulos.sen2")}</p>
           </div>
          
-          <div className='hidden sm:hidden md:inline-flex md:order-4 md:col-span-2'>
-            <img src ={ModulosSvg} alt='modulos'/>
-          </div>
+          
           <Fade triggerOnce>
             <div className='mx-auto'>
           
             
-              <img src={Dibujo} alt='Dibujo' className=' mx-auto mb-4'/>  
+              <img src={Dibujo} alt='Dibujo' className=' mx-auto mb-4 scale-90 ml-8'/>  
               
             </div>
           </Fade>
