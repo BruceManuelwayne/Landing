68d9029fac882c9ca7aee886020a6f2103225d9a
Enrique <quique.green92@gmail.com>
2022-12-22 17:24:31 -0300

    Update index.js

--- la-ola/src/components/Tech/index.js
+++ la-ola/src/components/Tech/index.js
@@ -18,22 +18,22 @@ const Tech = () => {
   <Fade triggerOnce fraction={0.6} >
   <div className='bg-slate-50 w-screen flex flex-wrap flex-col lg:pt-6 '>
   <div className=''>
-      <h1 className='text-[#0E63AB] font-CustomOla text-base lg:text-xl text-center mx-auto p-6 underline'>{t("tech.titulo")}</h1>
+      <h1 className='text-[#0E63AB] font-CustomOla text-base lg:text-xl text-center mx-auto p-6 md:pb-0 underline'>{t("tech.titulo")}</h1>
     </div>   
-    <div className='bg-slate-50 flex flex-col w-screen  lg:flex-row-reverse items-center md:justify-evenly text-center md:text-left'>
+    <div className='bg-slate-50 flex flex-col w-screen lg:flex-row-reverse items-center md:justify-evenly text-center md:text-left'>
      
-      <div className='px-8 pb-4  mx-auto md:p-20  w-screen font-TTNormsLight text-sm lg:text-base text-justify md:max-w-fit'>
+      <div className='px-10 pb-4  mx-auto md:p-8 xl:px-16  w-screen font-TTNormsLight text-sm lg:text-base text-justify md:max-w-1/2'>
         <Fade triggerOnce>{t("tech.tecno1")}</Fade>
       </div>
       
   <Fade/>
   
-      <div className='p-5 md:pl-36 md:p-10 hidden lg:inline w-1/2'>
-       <Fade triggerOnce> <img className='w-52 md:w-96  '
+      <div className='p-5 md:pl-36 md:p-2 hidden lg:inline xl:pl-40 w-1/2'>
+       <Fade triggerOnce> <img className='w-52 md:w-96 xl:scale-75  '
           src={Logo} alt='LogoLaOlaAzul'/></Fade>
       </div>
     </div>
-    <div className='bg-[#0E63AB] '>
+    <div className='hidden md:inline bg-[#0E63AB] '>
     <Fade triggerOnce direction='left' fraction={0.5}> <img className='w-screen p-5 lg:p-20 '
           src={Dibujo} alt='LogoLaOlaAzul'/></Fade>
    
