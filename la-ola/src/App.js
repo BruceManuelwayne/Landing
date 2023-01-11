6491acd379c6e4f31e434a7315d8fe2631496e1b
Enrique <quique.green92@gmail.com>
2023-01-11 09:40:11 -0300

    seo

--- la-ola/src/App.js
+++ la-ola/src/App.js
@@ -5,13 +5,36 @@ import { Routes,Route} from 'react-router-dom';
 import LandingPageLayout from './layouts/LandingPage';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { useTranslation } from 'react-i18next';
 
 
 
 function App() {
-
+  const[t] = useTranslation("translation"); 
   
   return (
+    <HelmetProvider>
+    <Helmet>
+    <title>
+      {t("seo.title")}
+    </title>
+    <meta
+            name="description"
+            content={t("seo.desc")}
+    />
+     <meta property="og:type" content="website" />
+          <meta property="og:url" content="www.designcode.io" />
+          <meta
+            property="og:title"
+            content={t("seo.title")}
+          />
+          <meta
+            property="og:description"
+            content={t("seo.desc")}
+          />
+          
+    </Helmet>
     <Suspense fallback={<div>loading...</div>}>
       
         <BrowserRouter>
@@ -23,6 +46,7 @@ function App() {
         </BrowserRouter>
       
     </Suspense>
+    </HelmetProvider>
   );
 }
 
